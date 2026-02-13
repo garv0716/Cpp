0f69fc1755a23a986f53b8f9dd6696c2c54a5862
Garv Gupta <garvg8423@gmail.com>
2026-02-13 13:50:59 +0000

    Majority Element in Array using stack

--- stack/nextGreaterElement.cpp
+++ stack/nextGreaterElement.cpp
@@ -1,12 +1,45 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
+void nextGreater(vector<int>&arr , vector<int>&ans){
+    int idx = arr.size() - 1;
+    ans[idx] = -1;
+
+    stack<int> s;
+    s.push(arr[idx]);
+    
+    for (idx = idx - 1; idx >= 0; idx-- ) {   // we start treversing the elements from last 2nd element 
+
+        int curr = arr[idx];
+        while(!s.empty() && curr >= s.top()){   // jab tak stack khasli na hoo jayee
+            s.pop();
+        }
+        if (s.empty()){
+            ans[idx] = -1;
+        } else{
+            ans [idx] = s.top();
+        }
+        s.push(curr);
+    }
+    for (int i = 0; i < arr.size(); i++){
+        cout << ans[i] << " ";
+        cout << endl;
+    }
+}
+
 int main(){
+
+    vector <int> arr = {6,8,0,1,3};
+    vector <int> ans = {0,0,0,0,0};
+    nextGreater(arr,ans);
 
     return 0;
 }
 
 /*
+
 =>Apporach
 
 arr = [2,3,5,66,7,5,3,5]        ans = [5,-1]
