248c70e2c794612ff5a6589ce417d139118e81f0
Garv Gupta <garvg8423@gmail.com>
2026-02-14 12:44:28 +0000

    update code

---
+++ Queue/1.queue.cpp
@@ -0,0 +1,11 @@
+// notes
+/*
+- FIFO data structure
+- elements should be added from last
+- element should be deleted form the front
+- operations  // O(1)
+ - push()[enqueue]: add elements from the end.
+ - pop() [dequeue]: delete elelments from the front       
+ - front(): It return the front element of the queue 
+
+*/
