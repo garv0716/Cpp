1b4a0d97ca611606860c7e1dcc9f429894a22b16
Garv Gupta <garvg8423@gmail.com>
2026-02-16 05:22:09 +0000

    Update Code.

---
+++ 6.GreedyAlgorithms/1.itro.cpp
@@ -0,0 +1,10 @@
+/*
+=> NOTES
+- it means make the greedy choice at all steps
+-choose the local optimum & hope for global optimum.
+- generally in greedy questions the use of sorting is much{hint}
+
+-> optimal substructur property
+  - a problem is said to have an optimal sub-structure if it can divided into sub-problems whose 
+    optimal solution are part of overall optimal solution.
+*/
---
+++ 6.GreedyAlgorithms/2.activitySelection.cpp
@@ -0,0 +1,8 @@
+/*
+=>Activity Selection {variation- max meetings in a room}
+select the max no. of activity that can be perform by a single person. 
+(a person can on one activity at a time)
+
+start = [10,12,20];
+end = [20, 25, 30];
+*/
