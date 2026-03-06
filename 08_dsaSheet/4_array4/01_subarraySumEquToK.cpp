b1ad68225d70adf97152fbf371e9ac4d94e6161a
Garv Gupta <garvg8423@gmail.com>
2026-03-06 23:59:54 +0530

    Refactor subarraySum function to improve readability and structure

--- 08_dsaSheet/4_array4/01_subarraySumEquToK.cpp
+++ 08_dsaSheet/4_array4/01_subarraySumEquToK.cpp
@@ -1,5 +1,33 @@
 #include<iostream>
+#include
 using namespace std;
+
+
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        vector<int>prefixSum(n, 0);
+
+        prefixSum[0] = 0;
+        for (int i = 1; i < n; i ++){
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }  
+
+        unordered_map<int, int> m;
+        for(int j = 0; j < n; j++){
+            if (prefixSum[j] == k) count ++;
+
+            int val = prefixSum[j] - k;
+            if (m.find(val) != m.end()){
+                count += m[val];
+            }
+            if (m.find(prefixSum[j]) == m.end()){
+                m[prefixSum[j]] = 0;
+            }
+            m[prefixSum[j]]++;
+        }
+        return count;
+    }  
 
 int main (){
 
