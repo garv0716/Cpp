7cd0699f50ad37e772416b025660e6550db166e9
Garv Gupta <garvg8423@gmail.com>
2026-03-10 19:52:50 +0530

    Add steps to implement the deletion of node

--- 10_binarySearchTree/03_deleteANode.cpp
+++ 10_binarySearchTree/03_deleteANode.cpp
@@ -1,7 +1,12 @@
 /*
 Cases for a node
  - no child(leaf node)
+   ->simply delete that root, then return null
  - 1 child
+   ->return the address of valid child, this will automaatically ignore that node
  - 2 child
+   ->replace val with inorder successor. means the data or node just after that node
+   ->delete inorder successor
+   ->the inorder successor will always have either 0 or 1 child.basically it does not have two childern
 */
 
