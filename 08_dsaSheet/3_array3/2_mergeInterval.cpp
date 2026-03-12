32ad4bac038004c77f8cdb4d1ab6601ad82b4057
Garv Gupta <garvg8423@gmail.com>
2026-03-12 22:43:29 +0530

    Add implementation of merge interval

--- 08_dsaSheet/3_array3/2_mergeInterval.cpp
+++ 08_dsaSheet/3_array3/2_mergeInterval.cpp
@@ -5,31 +5,7 @@
 using namespace std;
 
 
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        if(intervals.size() <= 1)
-            return intervals;
-
-        sort(intervals.begin(), intervals.end());
-
-        int index = 0;
-
-        for(int i = 1; i < intervals.size(); i++) {
-
-            if(intervals[i][0] <= intervals[index][1]) {
-                intervals[index][1] = max(intervals[index][1], intervals[i][1]);
-            }
-            else {
-                index++;
-                intervals[index] = intervals[i];
-            }
-        }
-
-        intervals.resize(index + 1);
-
-        return intervals;
-    }
-
+   
 int main() {
 
     vector<vector<int>> intervals = {
