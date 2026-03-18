8b680292fef27849514672d0020c40b1ffc7f085
Garv Gupta <garvg8423@gmail.com>
2026-03-18 23:09:26 +0530

    Add implementation of near by cars

--- 11_heap/05_nearbyCars.cpp
+++ 11_heap/05_nearbyCars.cpp
@@ -27,17 +27,13 @@ void nearbyCar(vector<pair<int, int>> pos, int k){
         int disSq = (pos[i].first * pos[i].first) + (pos[i].second* pos[i].second);
         cars.push_back(Car(i, disSq));
     }
-
     priority_queue<Car>pq(cars.begin(), cars.end()); // O(n)
-
     for (int i = 0; i < k; i++){
        cout<< "car:" << pq.top().idx;
        pq.pop(); //O(klogn)
     }
-
-
 }
-
+ 
 int main(){
     vector <pair<int , int>> pos;
     pos.push_back(make_pair(3, 3));
