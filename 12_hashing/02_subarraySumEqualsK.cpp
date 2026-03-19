c0298eef4d7b9246ad8ba5e3095f309309d80c93
Garv Gupta <garvg8423@gmail.com>
2026-03-19 23:42:54 +0530

    Add implementation of count the subarray sum equals to k

--- 12_hashing/02_subarraySumEqualsK.cpp
+++ 12_hashing/02_subarraySumEqualsK.cpp
@@ -6,10 +6,29 @@ using namespace std;
 
 
 int subarrSumEqlK(vector<int> arr, int k){
-    
+    unordered_map<int , int> m;
+    m[0] = 1;
+    int sum = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++){
+        sum += arr[i];
+        if (m.count(sum-k)){
+            ans += m[sum-k];
+        }
+
+        if (m.count(sum)){
+            m[sum]++;
+        }else{
+            m[sum] = 1;
+        }
+    }
+    return ans;
 }
 
 int main(){
-
+    vector<int>arr = {10, 2, -2, -20, 10};
+    int k = -10;
+    
     return 0;
 }
