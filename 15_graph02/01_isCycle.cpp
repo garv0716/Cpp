8d00d26e39c6ae15634bd3b434b059940bf45292
Garv Gupta <garvg8423@gmail.com>
2026-04-04 23:23:58 +0530

    add implementaion of isCycle in graph or not

--- 15_graph02/01_isCycle.cpp
+++ 15_graph02/01_isCycle.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<list>
+#include<vector>
 using namespace std;
 
 class Graph{
@@ -21,7 +22,35 @@ public:
           }
     }
 
-    bool dirCycleHelper(int sec, )
+    bool dirCycleHelper(int src, vector<bool> &vis, vector<bool> &recPath){
+        vis[src] = true;
+        recPath[src] = true;
+        list<int> neighbours = l[src];
+
+        for (int v : neighbours){
+            if (!vis[v]){
+                if (dirCycleHelper(v, vis, recPath)){
+                    return true;
+                }
+            } else if (recPath[v]){
+                return true;
+            }
+        }
+
+        recPath[src] = false;
+        return false;
+    }
+
+    void print(){
+        for (int u = 0; u < V; u++){
+            list<int> &neighbors = l[u];
+            cout << u << ":" ;
+            for(int v : neighbors){
+                cout << v << " ";
+            }
+            cout << endl;
+        }
+    }
 
 
     bool isCycleDirected(){
@@ -29,7 +58,7 @@ public:
         vector<bool> recPath(V, false);
 
         for (int i = 0; i < V; i++){
-            if (vis[i]){
+            if (!vis[i]){
                 if(dirCycleHelper(i, vis, recPath)){
                     return true;
                 }
@@ -38,9 +67,16 @@ public:
         return false;
 
     }
-     
 };
 
 int main(){
+    Graph g(4, false);
+    g.addEdge(1, 0);
+    g.addEdge(0, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 0);
+
+    cout << g.isCycleDirected()<< endl;
+
 
 }
