7bda3d1d863e8273f55975ea8b8bbbbe6394c583
Garv Gupta <garvg8423@gmail.com>
2026-04-10 23:17:46 +0530

    Add notes and algorithm for implementaiton of Prims algorithm

--- 17_graph04/03_primsAlgo.cpp
+++ 17_graph04/03_primsAlgo.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include <list>
+#include<queue>
+#include<vector>
+#include<stack>
 using namespace std;
 
 class Graph{
@@ -18,4 +21,46 @@ public:
             l[v].push_back(make_pair(u, wt));
         }
     }
+    void primsAlgo(int src){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        vector<bool>mst(V, false);
+
+        pq.push(make_pair(0, src));
+        int ans = 0;
+
+        while(pq.size() > 0){
+            int u = pq.top().second;
+            int wt = pq.top().first;
+            pq.pop();
+
+            if(!mst[u]){
+                mst[u] = true;
+                ans += wt;
+                list<pair<int, int>> neighbours = l[u];
+                for(pair<int, int> n : neighbours){
+                    int v = n.first;
+                    int currWt = n.second;
+                    pq.push(make_pair(currWt, v));
+                }
+                
+            }
+        }
+        cout << "Final Cost : " << ans << " ";
+
+    }
 };
+
+int main(){
+    //prims algo graph
+    Graph graph(4);
+
+    graph.addEdge(0, 1, 10);
+    graph.addEdge(0, 2, 15);
+    
+    graph.addEdge(1, 3, 30);
+
+    graph.addEdge(2, 3, 50);
+
+    graph.primsAlgo(0);
+}
