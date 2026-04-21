696852135a41cd955a32f21ca30436c5fc2538d8
Garv Gupta <garvg8423@gmail.com>
2026-04-21 11:16:11 +0530

    Add implementaion of 01Knapsack with memoization

--- 18_dp/02_01Knapsack.cpp
+++ 18_dp/02_01Knapsack.cpp
@@ -24,11 +24,41 @@ int knapS(vector<int> val, vector<int> wt, int W, int n){
     }
 }
 
+
+// with Memoization
+
+int knapMemo(vector<int> val, vector<int> wt, int W, int n, vector<vector<int>>&dp){ //O(n * W)
+    // base case
+    if (n == 0 || W == 0){
+        return 0;
+    }
+    if (dp[n][W] != -1){
+        return dp[n][W];
+    }
+
+    int itemWt = wt[n-1];
+    int itemval = val[n-1];
+    //include
+    if (itemWt <= W){
+        // include
+        int ans1 = knapMemo(val, wt, W - itemWt, n-1, dp) + itemval;
+        //exclude
+        int ans2 = knapMemo(val, wt, W, n-1, dp);
+        dp[n][W] =  max(ans1, ans2);
+    // exclude
+    }else{
+        dp[n][W] =  knapMemo(val, wt, W, n-1, dp);
+    }
+    return dp[n][W];
+}
+
 int main(){
     vector<int> val = {15, 14, 10, 45, 30};
     vector<int> wt = {2,5,1,3,4};
     int W = 7;
     int n = 5;
-    cout << knapS(val, wt, W, n);
+    vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
+    cout << knapMemo(val, wt, W, n, dp);
     return 0;
 }
+    
