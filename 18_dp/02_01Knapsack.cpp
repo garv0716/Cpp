934c6e2178742c374146f4f8dae8d68586be646d
Garv Gupta <garvg8423@gmail.com>
2026-04-21 23:44:27 +0530

    Add implementation of knapsack by using tabulation

--- 18_dp/02_01Knapsack.cpp
+++ 18_dp/02_01Knapsack.cpp
@@ -28,10 +28,10 @@ int knapS(vector<int> val, vector<int> wt, int W, int n){
 // with Memoization
 
 int knapMemo(vector<int> val, vector<int> wt, int W, int n, vector<vector<int>>&dp){ //O(n * W)
-    // base case
     if (n == 0 || W == 0){
         return 0;
     }
+
     if (dp[n][W] != -1){
         return dp[n][W];
     }
@@ -49,6 +49,35 @@ int knapMemo(vector<int> val, vector<int> wt, int W, int n, vector<vector<int>>&
     }else{
         dp[n][W] =  knapMemo(val, wt, W, n-1, dp);
     }
+
+    return dp[n][W];
+}
+
+
+
+// in knapsack tabulation it is guranteed that we traverse each and every cell, so there is valid value for each cell 
+
+int knapsackTab(vector<int> val, vector<int> wt, int W, int n){
+    vector<vector<int>> dp(n+1, vector<int>(W+1, 0));
+    for(int i = 1; i < n+1; i++){
+        for(int j = 1; j < W+1; j++){
+          int itemWt = wt[i-1];
+          int itemVal = val[i-1];
+
+          if (itemWt <= j){
+            dp[i][j] = max(itemVal + dp[i-1][j - itemWt], dp[i-1][j]);
+          }else{
+            dp[i][j] = dp[i-1][j];
+          }
+
+        }
+    }
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < W+1; j++){
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
     return dp[n][W];
 }
 
@@ -59,6 +88,15 @@ int main(){
     int n = 5;
     vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
     cout << knapMemo(val, wt, W, n, dp);
+    cout << "mem matrix\n";
+      for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < W+1; j++){
+            cout << dp[i][j] << " " ;
+        }
+        cout << endl;
+    }
+    cout << "---------------------" << endl;
+    cout << knapsackTab(val, wt, W, n);
     return 0;
 }
     
