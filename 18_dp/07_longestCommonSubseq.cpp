ca9507cdec4fb793c3aa75b357b87a28ccfc59f8
Garv Gupta <garvg8423@gmail.com>
2026-04-29 23:12:09 +0530

    Add implementation for longest common subsequence

--- 18_dp/07_longestCommonSubseq.cpp
+++ 18_dp/07_longestCommonSubseq.cpp
@@ -2,12 +2,47 @@
 #include<vector>
 using namespace std;
 
+int lcs(string str1, string str2, vector<vector<int>>dp, int n, int m){ // O(n*m)
+
+    if (str1.size() == 0 ||  str2.size() == 0){
+        return 0;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = i; j <= m; j++){
+
+        }
+    }
+}
+
+
+int lcsTab(string str1, string str2, vector<vector<int>>dp, int n, int m){
+
+
+     if (str1.size() == 0 ||  str2.size() == 0){
+        return 0;
+    }
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0 ));
+
+    for(int i = 1; i < n; i++){
+        for (int j = 1; j < m; j++){
+            if (str1[i-1] == str2[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+    }
+    return dp[n][m];
+
+
+}
+
 int main(){
 
-    string str1 = "";
-    string str2 = "";
-    int n = str1.size();
-    int m = str2.size();
+    string str1 = "abcdge";
+    string str2 = "abedg";
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+
 
     return 0;
 }
