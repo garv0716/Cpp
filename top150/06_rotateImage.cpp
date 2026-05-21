7edb28358dbe8740ce71d6057a53a7854b22147e
Garv Gupta <garvg8423@gmail.com>
2026-05-21 23:45:59 +0530

    Add implementation of rotate image

--- top150/06_rotateImage.cpp
+++ top150/06_rotateImage.cpp
@@ -2,9 +2,42 @@
 #include<vector>
 using namespace std;
 
+    // Function to rotate matrix 90 degrees clockwise in-place
+    void rotateClockwise(vector<vector<int>>& matrix) {
+        int n = matrix.size();
 
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                // Swap element at (i, j) with (j, i) to transpose
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
 
-int main(){
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; ++i) {
+            // Reverse the current row to complete clockwise rotation
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+// Driver code
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    
+    rotateClockwise(matrix);
+
+    // Print rotated matrix
+    for (auto row : matrix) {
+        for (int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
