c0bf377a5b1626490e568e4f76f47f152af7246b
Garv Gupta <garvg8423@gmail.com>
2026-05-28 23:56:16 +0530

    Add implementation of Maximum point you can obtain from cards

---
+++ top150/10_maxPoint.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    // Function to return maximum score by picking k cards from either end
+    int maxScore(vector<int>& cardPoints, int k) {
+        // Get the total number of cards
+        int n = cardPoints.size();
+
+        // Calculate initial sum by picking first k cards from front
+        int total = 0;
+        for (int i = 0; i < k; ++i) {
+            total += cardPoints[i];
+        }
+
+        // Store current max score
+        int maxPoints = total;
+
+        // Move the window from front to back k times
+        for (int i = 0; i < k; ++i) {
+            // Subtract card from front
+            total -= cardPoints[k - 1 - i];
+
+            // Add card from back
+            total += cardPoints[n - 1 - i];
+
+            // Update max score if needed
+            maxPoints = max(maxPoints, total);
+        }
+
+        // Return the best score
+        return maxPoints;
+    }
+};
+
+// Driver code
+int main() {
+    // Example input
+    vector<int> cards = {1, 2, 3, 4, 5, 6, 1};
+    int k = 3;
+
+    // Create object of Solution class
+    Solution sol;
+
+    cout << sol.maxScore(cards, k) << endl;
+
+    return 0;
+}
