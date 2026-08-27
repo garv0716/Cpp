9c2406c30e81d95ee6570f965b558cd056c0c50a
Garv Gupta <garvg8423@gmail.com>
2026-08-27 23:53:42 +0530

    Add implementation for climbing stairs problem

---
+++ 18_dp/13_climbiStair.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int prev2 = 1; // ways to reach (i-2)th step
+        int prev1 = 2; // ways to reach (i-1)th step
+        int current = 0;
+
+        for (int i = 3; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n = 5;
+    std::cout << "Distinct ways to climb " << n << " steps: " 
+              << sol.climbStairs(n) << std::endl;
+    return 0;
+}
